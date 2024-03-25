e294d2d56b8ac2e35aef66c33fe4b31ce850616c
Blessto296 <blesstofearethd@gmail.com>
2024-03-25 12:59:50 +0530

    a

---
+++ javascript.js
@@ -0,0 +1,57 @@
+console.log("Hello javascript")
+console.log(1+2)
+//const ,var, let
+const pi=3.1415;
+//pi=6.28
+var x=1;
+var y=2;
+var z="hello";
+var a=[x,y,z];
+console.log(a)
+var lst =[1,"0",[1,2],4,8,true];
+console.log(lst)
+var obj={
+    name :"mr y",
+    job :"hero",
+    age :"20",
+    address :"singapore"
+}
+console.log(obj)
+ var phone=
+ {
+name:"redmi", 
+size:"12gb ram,250 storage",
+color:["green","red","blue"]
+}
+console.log(phone)
+let str1="hello";
+let str2="world";
+let str3=`hello
+world`;
+console.log(str1)
+console.log(str2)
+console.log(str3)
+ 
+
+let outcome=[[1,2,3,4,5,6], 
+             [2,3,4,5,6,7],
+             [3,4,5,6,7,8],
+             [4,5,6,7,8,9],
+             [5,6,7,8,9,0],
+             [6,7,8,9,0,1]]
+console.log(outcome[1][3])  
+
+let d3=[ 
+[ 
+    [1,2],
+    [1,2]
+],
+[
+    [1,2,],
+    [1,2]
+],
+[
+[1,2],
+[1,2]
+]
+]
